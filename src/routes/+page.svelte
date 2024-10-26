bb59000e760d373bad9ba58d3f514c9fd25bc500
Iver Smage <iversmage@gmail.com>
2024-10-26 15:46:16 +0200

    Fix random issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,80 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
+  import random, { Random } from "random";
+  import seedrandom from "seedrandom";
 
-  let { data } = $props();
-  let bingoboard = $derived(data.bingoboard);
-  $inspect(data);
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("");
+
+  function random_string(n: number) {
+    let op = "";
+    for (let i = 0; i <= n; i++) {
+      let choice = random.choice(characters);
+      op += choice;
+    }
+    return op;
+  }
+
+  interface BINGO {
+    B: Number;
+    I: Number;
+    N: Number;
+    G: Number;
+    O: Number;
+  }
+
+  const ztoftn = [...Array(15).keys()];
+  const B = ztoftn.map((n) => n + 1);
+  const I = ztoftn.map((n) => n + 16);
+  const N = ztoftn.map((n) => n + 31);
+  const G = ztoftn.map((n) => n + 46);
+  const O = ztoftn.map((n) => n + 61);
+
+  function pick_n<T>(rng: Random, lst: Array<T>, n: number): Array<T> {
+    const op: Array<T> = [];
+    for (let i = 0; i <= n; i++) {
+      let choice = rng.choice(lst);
+      while (!choice || op.includes(choice)) {
+        choice = rng.choice(lst);
+      }
+      op.push(choice);
+    }
+    return op;
+  }
+
+  function get_bingo_board(url: URL) {
+    const seed = url.searchParams.get("seed");
+
+    if (seed === null) {
+      const seed = random_string(100);
+      const date = new Date();
+      goto(`./?seed=${seed}${date.getMilliseconds()}`);
+      return [];
+    }
+
+    const rng = new Random(seedrandom(seed));
+    const bs = pick_n(rng, B, 5);
+    const is = pick_n(rng, I, 5);
+    const ns = pick_n(rng, N, 5);
+    const gs = pick_n(rng, G, 5);
+    const os = pick_n(rng, O, 5);
+
+    const bingoboard: Array<BINGO> = [];
+    for (let i of Array(5).keys()) {
+      bingoboard.push({
+        B: bs.at(i)!,
+        I: is.at(i)!,
+        N: ns.at(i)!,
+        G: gs.at(i)!,
+        O: os.at(i)!,
+      });
+    }
+
+    return bingoboard;
+  }
+
+  let bingoboard: BINGO[] = $state([]);
 
   function serialize_square(name: string, index: number): string {
     return `${name}${index}`;
@@ -17,7 +87,7 @@
     } else {
       query.set(serialize_square(name, index), "checked");
     }
-    goto(`?${query.toString()}`);
+    goto(`./?${query.toString()}`);
   }
 
   function is_checked(name: string, index: number): boolean {
@@ -25,13 +95,17 @@
     const res = query.get(serialize_square(name, index)) === "checked";
     return res;
   }
+
+  $effect(() => {
+    bingoboard = get_bingo_board($page.url);
+  });
 </script>
 
 <div
   class="flex justify-between flex-row w-full flex-grow mt-4 align-bottom mb-4"
 >
   <h1 class="font-light text-4xl text-slate-800">Bingo</h1>
-  <a href="/" class="text-sky-800">Nytt brett</a>
+  <a href="./" class="text-sky-800">Nytt brett</a>
 </div>
 
 <div
